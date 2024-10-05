d3a6f7118dcabb8e1b93e0aed972cc739f3dbf29
Abdullah02020 <eng.abdullah.1346@gmail.com>
2024-10-05 11:12:21 +0300

    ييي

--- components/ui/FaqItem.tsx
+++ components/ui/FaqItem.tsx
@@ -1,7 +1,5 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
@@ -41,11 +39,11 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
+      <div>
         {activeId === item.id && (
           <div className="body-3 px-7 py-3.5">{item.answer}</div>
         )}
-      </SlideDown>
+      </div>
 
       <div
         className={clsx(
