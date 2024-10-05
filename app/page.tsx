7c1e7ef22eaa923d277699afc39f383c70193510
Abdullah02020 <eng.abdullah.1346@gmail.com>
2024-10-05 11:15:35 +0300

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Download from "@/components/Download";
-import Faq from "@/components/Faq";
-import Features from "@/components/Features";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
+import Download from "../components/Download";
+import Faq from "../components/Faq";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
 import Image from "next/image";
 
 export default function Home() {
